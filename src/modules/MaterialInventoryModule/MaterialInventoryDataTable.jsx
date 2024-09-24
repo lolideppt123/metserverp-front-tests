29b9e48a9ee2b36c2b4c97f5b87219b9db3d1eb3
Your Name <you@example.com>
2024-09-24 09:41:47 -0400

    added material inventory mobile feature

--- src/modules/MaterialInventoryModule/MaterialInventoryDataTable.jsx
+++ src/modules/MaterialInventoryModule/MaterialInventoryDataTable.jsx
@@ -1,19 +1,12 @@
-import moment from 'moment';
-import { NavLink } from 'react-router-dom';
-import { Spin, Flex } from 'antd';
-
-import MoneyFormatter, { NumberFormatter } from '../../settings/MoneyFormatter';
+import { Table } from 'antd';
+import Spinner from '../../components/Fallback/Spinner';
 
 export default function MaterialInventoryDataTable({ config }) {
-    const { dataTableColumn, data, loading } = config;
+    const { newDataColumn, data, loading, tableWidth } = config;
     return (
-        <div className="contianer">
+        <div className="contianer-fluid app-table-container">
             {loading ? (
-                <div className="py-5">
-                    <Flex vertical>
-                        <Spin />
-                    </Flex>
-                </div>
+                <Spinner />
             ) : (
                 <>
                     {!data?.length ? (
@@ -21,35 +14,20 @@ export default function MaterialInventoryDataTable({ config }) {
                             <h6 className="text-center px-3 mt-4 mb-1"><i>Nothing to display yet</i></h6>
                         </div>
                     ) : (
-                        <div className="app-table">
-                            <table className="table table-striped table-hover">
-                                <thead>
-                                    <tr>
-                                        {dataTableColumn.map((value) => (
-                                            <th className='text-center' key={value.key}>{value.title}</th>
-                                        ))}
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {data?.map((item, index) => (
-                                        <tr key={index}>
-                                            <td className='text-center'>{(moment(item.last_ordered_date)).format("MMM DD, YYYY")}</td>
-                                            <td className='col col-lg-3 text-center'><NavLink to={`transaction/${item.material_name}`}>{item.material_name}</NavLink> </td>
-                                            {/* <td><NumberFormatter amount={item.total_inventory} /></td> */}
-                                            <td className='col col-lg-2'>
-                                                <div className="progress" style={{ height: "20px", border: "1px solid grey" }}>
-                                                    <div className={`progress-bar bg-${item.color} bg-gradient`} role="progressbar" style={{ width: item.width }}>
-                                                        <span><NumberFormatter amount={item.total_inventory} /></span>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className='text-center'><MoneyFormatter amount={item.unit_price} /> | {item.stock_left}</td>
-                                            <td className='text-center'>{item.material_unit}</td>
-                                            <td className='text-center'>{(moment(item.order_update)).format("MMM DD, YYYY")}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                        <div className="app-table material-inventory-data-table">
+                            <Table
+                                dataSource={data}
+                                columns={newDataColumn}
+                                rowKey={(data) => data.pk}
+                                scroll={{
+                                    x: tableWidth ? tableWidth : 900,
+                                    y: null
+                                }}
+                                pagination={{
+                                    pageSize: 10,
+                                    showSizeChanger: true
+                                }}
+                            />
                         </div>
                     )}
                 </>
--- src/pages/MaterialInventory/MaterialInventory.jsx
+++ src/pages/MaterialInventory/MaterialInventory.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useAxiosFunction from '../../hooks/useAxiosFunction';
 import Spinner from '../../components/Fallback/Spinner';
 import NoServerResponse from '../../components/Errors/NoServerResponse';
@@ -7,42 +7,115 @@ import GetStartedTemplate from '../../components/Fallback/GetStartedTemplate';
 import DataTablePageHeader from '../../modules/FspPanelModule/DataTablePageHeader';
 import MaterialInventoryDataTable from '../../modules/MaterialInventoryModule/MaterialInventoryDataTable';
 
+import { NumberFormatter } from '../../settings/MoneyFormatter';
+import dayjs from 'dayjs';
+import { NavLink } from 'react-router-dom';
+import RenderText from '../../components/Tooltip/RenderText';
+
+import { useSelector } from 'react-redux';
+import { selectDrawerPlatform } from '../../features/drawer/drawerSlice';
+import { Tooltip } from 'antd';
+
+
 export default function MaterialInventory() {
+    const { isMobile } = useSelector(selectDrawerPlatform);
     const { loading, response: data, error, axiosFetch } = useAxiosFunction();
+    const [newDataColumn, setNewDataColumn] = useState([]);
+    const [tableWidth, setTableWidth] = useState(null);
+
     const Labels = {
         BASE_ENTITY: 'Inventory',
         TABLE_TITLE: 'Material Inventory',
-        ADD_NEW_ENTITY: 'Add New Material Inventory',
+        ADD_NEW_ENTITY: 'Add Material Inventory',
         NEW_ENTITY_URL: 'inventory/materials/add',
     }
     const dataTableColumn = [
         {
-            title: 'Last Order',
-            key: 'orderedDate'
+            title: <div className="fs-md fw-semibold text-center">Last Order</div>,
+            key: 'orderedDate',
+            dataIndex: "last_ordered_date",
+            width: 125,
+            render: (date, record) => {
+                return (
+                    <div className={`fs-md fw-semibold text-center`}>
+                        {dayjs(date).format("MMM DD, YYYY")}
+                    </div>
+                );
+            },
         },
         {
-            title: 'Product',
-            key: 'product'
+            title: <div className={`fs-md fw-semibold text-center`}>Material Name</div>,
+            key: "materialName",
+            dataIndex: "material_name",
+            fixed: "left",
+            width: 250,
+            render: (text, record) => {
+                return (
+                    <div className={`fs-md fw-semibold text-uppercase text-center`}>
+                        <NavLink to={`transaction/${encodeURIComponent(text)}`}>
+                            {<RenderText text={text} maxLength={20} />}
+                        </NavLink>
+                    </div>
+                );
+            },
         },
         {
-            title: 'Current Stock',
-            key: 'currentStock'
+            title: <div className="fs-md fw-semibold text-center">Current Stock</div>,
+            key: 'currentStock',
+            dataIndex: "stock_left",
+            width: 175,
+            render: (text, record) => {
+                return (
+                    <div className="progress" style={{ height: "20px", border: "1px solid grey" }}>
+                        <div className={`progress-bar bg-${record.color} bg-gradient`} role="progressbar" style={{ width: record.width }}>
+                            <span><NumberFormatter amount={text} /></span>
+                        </div>
+                    </div>
+                );
+            },
         },
         {
-            title: 'Unit Price',
-            key: 'unitPrice'
+            title: <div className="fs-md fw-semibold text-center">Unit Price</div>,
+            key: 'unitPrice',
+            dataIndex: "unit_price",
+            width: 200,
+            render: (text, record) => {
+                return (
+                    <div className={`fs-md fw-semibold text-uppercase text-center`}>
+                        ${<NumberFormatter amount={text} />} | {record.stock_left}
+                    </div>
+                );
+            },
         },
-        {
-            title: 'Unit',
-            key: 'productUnit'
-        },
+        // {
+        //     title: <div className="fs-md fw-semibold text-center">Unit</div>,
+        //     key: 'materialUnit',
+        //     dataIndex: "material_unit",
+        //     render: (text, record) => {
+        //         return (
+        //             <div className={`fs-md fw-semibold text-uppercase text-center`}>
+        //                 {text}
+        //             </div>
+        //         );
+        //     },
+
+        // },
         // {
         //     title: 'Last Order',
         //     key: 'orderedDate'
         // },
         {
-            title: 'Updated',
-            key: 'updated'
+            title: <div className="fs-md fw-semibold text-center">Updated</div>,
+            key: 'orderUpdate',
+            dataIndex: "order_update",
+            width: 125,
+            render: (text, record) => {
+                return (
+                    <div className={`fs-md fw-semibold text-center`}>
+                        {dayjs(text).format("MMM DD, YYYY")}
+                    </div>
+                );
+            },
         },
     ]
 
@@ -57,12 +130,83 @@ export default function MaterialInventory() {
         getData();
     }, [])
 
+    useEffect(() => {
+        if (isMobile) {
+            setNewDataColumn([
+                {
+                    title: <div className={`fs-md fw-semibold text-start`}>Material Name</div>,
+                    key: "materialName",
+                    dataIndex: "material_name",
+                    // fixed: "left",
+                    // width: 250,
+                    render: (text, record) => {
+                        return (
+                            <>
+                                <div className={`fs-md fw-semibold text-uppercase text-start`} style={{ fontSize: '16px' }}>
+                                    <NavLink to={`transaction/${encodeURIComponent(text)}`}>
+                                        {<RenderText text={text} maxLength={20} />}
+                                    </NavLink>
+                                </div>
+
+                                <div className="text-start mt-1" style={{ fontSize: '13px' }}>
+                                    <span className='fw-semibold'>Last Order: </span>
+                                    <span>{dayjs(record.last_ordered_date).format("MMM DD, YYYY")}</span>
+                                </div>
+                                <div className="text-start mt-1" style={{ fontSize: '13px' }}>
+                                    <span className='fw-semibold'>Updated: </span>
+                                    <span>{dayjs(record.order_update).format("MMM DD, YYYY")}</span>
+                                </div>
+                            </>
+                        );
+                    },
+                },
+                {
+                    title: <div className="fs-md fw-semibold text-center">Current Stock</div>,
+                    key: 'currentStock',
+                    dataIndex: "stock_left",
+                    width: 125,
+                    render: (text, record) => {
+                        return (
+                            <>
+                                <div className="fs-md fw-semibold text-uppercase text-end">
+                                    <Tooltip className='pointer' title={
+                                        <>
+                                            <NumberFormatter amount={record.total_inventory} /> {record.product_unit}
+                                        </>
+                                    }>
+                                        <span style={{ fontSize: '13px', padding: '8px 14px' }} className={`badge bg-${record.color} fw-semibold bg-gradient`}>
+                                            <NumberFormatter amount={record.total_inventory} />
+                                        </span>
+                                    </Tooltip>
+                                </div>
+
+                                <div className="text-end mt-1 fw-semibold" style={{ fontSize: '12px' }}>
+                                    ${<NumberFormatter amount={record.unit_price} />} | {record.stock_left}
+                                </div>
+                            </>
+                        );
+                    },
+                },
+            ])
+
+
+            setTableWidth("max-content");
+        }
+        else {
+            setNewDataColumn(dataTableColumn);
+            setTableWidth(null);
+        }
+    }, [isMobile])
+
+    console.log(data)
+
     const config = {
-        dataTableColumn,
+        newDataColumn,
         Labels,
         data,
         loading,
         error,
+        tableWidth
     }
     return (
         <>
